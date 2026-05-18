acb0745545a8aa6f35b77d7d7dd7d4c8a07028a5
Harvir-T <harvir.thind@gmail.com>
2026-05-18 18:35:44 -0400

    Fix MoM% column references in staging and correct active accounts aggregations in mart_igaming_yearly_trend

--- models/mart/mart_igaming_yearly_trend.sql
+++ models/mart/mart_igaming_yearly_trend.sql
@@ -4,7 +4,7 @@ SELECT
 EXTRACT(YEAR FROM DATE(year_month || '-01')) AS fiscal_year,
 SUM(cash_wagers_m) AS total_cash_wagers_m,
 SUM(naggr_m) AS total_naggr_m,
-AVG(active_player_accounts_k) AS average_active_player_accounts_k,
-AVG(arppa_cad) AS average_arppa_cad
+SUM(active_player_accounts_k) AS total_active_player_accounts_k,
+SUM(naggr_m)/SUM(NULLIF(active_player_accounts_k, 0)) AS average_arppa_cad
 FROM {{ ref('stg_igaming_ontario') }}
 GROUP BY EXTRACT(YEAR FROM DATE(year_month || '-01'))
--- models/staging/stg_igaming_ontario.sql
+++ models/staging/stg_igaming_ontario.sql
@@ -2,11 +2,11 @@ SELECT
 FiscalYearQuarter AS fiscal_year_quarter,
 YearMonth AS year_month,
 CashWagers_M AS cash_wagers_m,
-'CashWagersMoM%' AS cash_wagers_mom,
+`CashWagersMoM%` AS cash_wagers_mom,
 NAGGR_M AS naggr_m,
-'NAGGRMoM%' AS naggr_mom,
+`NAGGRMoM%` AS naggr_mom,
 ActivePlayerAccounts_K AS active_player_accounts_k,
-'ActivePlayerAccountsMoM%' AS active_player_accounts_mom,
+`ActivePlayerAccountsMoM%` AS active_player_accounts_mom,
 ARPPA_CAD AS arppa_cad,
-'ARPPA_MoM%' AS arppa_mom
+`ARPPAMoM%` AS arppa_mom
 FROM {{ source('raw', 'igaming_ontario') }}
